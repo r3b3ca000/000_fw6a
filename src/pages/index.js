8b3c3d61e433c79fbf08a814fc6d1b87fdaa1fb6
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2024-09-30 14:32:50 +0000

    Aula 30-09

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,24 @@
 import Footer from "./modulos/footer";
 import Navbar from "./modulos/navbar";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Index() {
   return <div>
     <Navbar />
     Bem Vindos
+    <Container>
+      <Row>
+        <Col className="bg-primary">1 of 2</Col>
+        <Col className="bg-danger">2 of 2</Col>
+      </Row>
+      <Row>
+        <Col className="bg-info">1 of 3</Col>
+        <Col className="bg-dark text-white">2 of 3</Col>
+        <Col className="bg-warning ">3 of 3</Col>
+      </Row>
+    </Container>
     <Footer />
   </div>;
 }
