5ddff46784e713251c17d78ca9bc98c2e7a4c259
r3b3ca000 <rebeca.brito@estudante.ifms.edu.br>
2024-10-07 14:15:50 +0000

    aulinha com mais cores

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,13 @@ export default function Index() {
     Bem Vindos
     <Container>
       <Row>
-        <Col className="bg-primary">1 of 2</Col>
-        <Col className="bg-danger">2 of 2</Col>
+        <Col className="bg-info bg-gradient">1 of 2</Col>
+        <Col className="bg-info bg-gradient">2 of 2</Col>
       </Row>
       <Row>
-        <Col className="bg-info">1 of 3</Col>
-        <Col className="bg-dark text-white">2 of 3</Col>
-        <Col className="bg-warning ">3 of 3</Col>
+        <Col className="bg-info-subtle">1 of 3</Col>
+        <Col className="bg-info-subtle">2 of 3</Col>
+        <Col className="bg-info-subtle">3 of 3</Col>
       </Row>
     </Container>
     <Footer />
--- src/pages/modulos/navbar.js
+++ src/pages/modulos/navbar.js
@@ -1,4 +1,18 @@
-function Navbar() {
-    return<div>Menu FW</div>    
+import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
+
+export default function Menu() {
+  return (
+    <Navbar expand="sn" className="bg-info">
+      <Container>
+        <Navbar.Brand className="text-white" href="#home">6A</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
-export default Navbar;
